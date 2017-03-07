9ecab6044bf9997a364320600a0edd2280832fa4
Pedro A. Alonso Baigorri <pedro.baigorri@gmail.com>
2017-03-07 17:13:32 +0100

    plot3

---
+++ plot3.R
@@ -0,0 +1,59 @@
+##############################################################################
+## File: plot1.R
+##
+## Description: Course_Project 2: Exloratory Data Analysis
+## https://www.coursera.org/learn/exploratory-data-analysis/peer/b5Ecl/course-project-2
+##
+## Exercise: Of the four types of sources indicated by the type (point, nonpoint, onroad, nonroad)
+##           variable, which of these four sources have seen decreases in emissions from 1999-2008 
+##           for Baltimore City? Which have seen increases in emissions from 1999-2008?
+##           Use the ggplot2 plotting system to make a plot answer this question.
+##
+## Author: Pedro A. Alonso Baigorri
+##############################################################################
+
+# setting the working path
+setwd("D://GIT_REPOSITORY//Exploratory_Course_Pract_2")
+
+
+# Load required libraries
+library(dplyr)
+library(stringr)
+library(ggplot2)
+
+
+# Getting the data
+localFile <- "./data/airPollution.zip"
+
+
+if (!file.exists(localFile))
+{
+    fileURL <-  "https://d396qusza40orc.cloudfront.net/exdata%2Fdata%2FNEI_data.zip"
+    
+    if (!file.exists("./data")){dir.create("./data")}
+
+    download.file(fileURL, localFile)
+    
+    unzip(localFile, exdir = "./data")    
+}
+
+## Reading data
+NEI <- readRDS("./data/summarySCC_PM25.rds")
+SCC <- readRDS("./data/Source_Classification_Code.rds")
+
+#converting year
+NEI$year <- as.factor(NEI$year)
+
+# Filtering for Baltimore
+bal <- subset(NEI, fips == "24510") 
+
+# agreggating by date & type
+a <- aggregate(Emissions ~ year + type , data = bal, FUN = sum)
+
+
+# drawing the plot 
+png(file = "plot3.png", width=480, height=480)
+qplot(year, Emissions, data = a, geom = "line", color = type)
+#barplot(a$Emissions, names.arg=a$year,  ylab = "Total Emissions", xlab = "year", main ="Yearly evolution of PM2.5 emissions in Baltimore")
+dev.off()
+
